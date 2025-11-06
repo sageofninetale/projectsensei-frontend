695b523cc82a57c553a064c94e091f9b279f8024
Aryan Subhash <aryansubhash19@gmail.com>
2025-11-06 06:30:35 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = 'force-no-store';
+export const runtime = 'nodejs';
 
 export const metadata = {
   title: 'ProjectSensei',
