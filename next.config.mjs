1e1ac8030baa733b7af077ccbefc94070e2817e2
Aryan Subhash <aryansubhash19@gmail.com>
2025-11-06 06:25:30 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // temporary while stabilising:
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  experimental: { typedRoutes: false }
+
+  // ensure we ship a normal Node server build (not static export)
+  // and avoid quirky optimisations while we finish auth
+  output: 'standalone',
+  experimental: { typedRoutes: false },
 };
+
 export default nextConfig;
