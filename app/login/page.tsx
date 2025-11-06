db85dae61d0ff893e9858a0ceb4d5e231436f08f
Aryan Subhash <aryansubhash19@gmail.com>
2025-11-06 05:04:48 +0000

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,3 @@
 export default function Login() {
-  return <main style={{padding: 40}}><h1>Login (placeholder)</h1></main>;
+  return <main style={{padding:40}}><h1>Login (placeholder)</h1></main>;
 }
